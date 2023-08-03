0c57f97c571c58bbb04abd3a5fa56b21712edd28
Ritik Gupta <ritikgupta856@gmail.com>
2023-08-03 16:34:06 +0530

    updated

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,3 +1,59 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:255e8183545bc904fc7e29c67f0e62ef12f17c42b79fa1271e2bc17009207a78
-size 1223
+import React from 'react';
+import './Avatar.css';
+
+function Avatar({ size, onClick, user }) {
+
+  let avatarSize;
+  let avatarTextSize;
+
+  if (size === 'small') {
+    avatarSize = '32px';
+  } else if (size === 'medium') {
+    avatarSize = '36px';
+  } else if (size === 'large') {
+    avatarSize = '45px';
+  } else if (size === 'x-large') {
+    avatarSize = '65px';
+    avatarTextSize = '2rem';
+  } else if (size === 'xx-large') {
+    avatarSize = '96px';
+    avatarTextSize = '4rem';
+  }
+
+
+
+  const avatarStyle = {
+    "--avatar-size": avatarSize,
+    "--avatar-text-size": avatarTextSize,
+    "--avatar-background-color": user?.color || "",
+  };
+
+  
+
+  return (
+    <div className="avatar" onClick={onClick} >
+      {user?.isOnline && (<>
+        {(size === "large") && <span className='status'></span>}
+      </>)}
+   
+      {user?.photoURL ? (
+        <img
+          className="avatar-image"
+          src={user?.photoURL}
+          alt=""
+          style={avatarStyle}
+        />
+      ) : (
+        <div className="avatar-text" style={avatarStyle}>
+          {user?.displayName?.charAt(0)}
+        </div>
+      )}
+
+
+
+    </div>
+
+  );
+};
+
+export default Avatar;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,18 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1c1c0642c634d64b25671f9fd5df539dba461fd72dcdcc09a1b92c5cc848166b
-size 243
+import "../pages/home.css";
+
+
+import Chats from './Chats';
+import React from "react";
+function Sidebar() {
+    return (
+
+        <div className="sidebar">
+            <Chats/>
+        </div>
+
+
+
+    );
+}
+
+export default Sidebar;
