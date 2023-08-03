672a113fc5892aa35b03f74bb4715c286dd29f1d
Ritik Gupta <ritikgupta856@gmail.com>
2023-08-03 12:31:36 +0530

    first commit

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,3 +1,76 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:28c90b504df69026d031265dde4a53d79ca06272128725a9286dfc40660787f5
-size 1705
+import { createContext, useEffect, useState } from "react";
+import React from 'react';
+import { auth, db } from "../firebase";
+import { onAuthStateChanged,signOut as authSignOut  } from "firebase/auth";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
+
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [isloading, setIsloading] = useState(true);
+  
+  const clear = async () => {
+    try {
+
+         if (currentUser) {
+        await updateDoc(doc(db, "users", currentUser.uid), {
+          isOnline: false,
+        });
+      }
+  
+
+      setCurrentUser(null);
+      setIsloading(false);
+  
+   
+  
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const authStateChanged = async (user) => {
+    setIsloading(true);
+    if(!user){
+       clear();
+       return;
+    }
+
+
+    const userDocExist = await getDoc(doc(db,"users" ,user.uid));
+    if(userDocExist.exists()){
+      await updateDoc(doc(db,"users",user.uid),{
+        isOnline:true,
+      });
+    }
+
+    const userDoc = await getDoc(doc(db,"users",user.uid));
+    setCurrentUser(userDoc.data());
+    setIsloading(false);
+  };
+
+  const signOut = () =>{
+    authSignOut(auth).then(()=>clear());
+  }
+
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, authStateChanged)
+     return () => unsub();
+  
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        currentUser,
+        setCurrentUser,
+        isloading,
+        setIsloading,
+        signOut,
+      }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
